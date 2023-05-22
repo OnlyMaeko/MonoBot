ff3c0e0769cb5800bbf52cee286f9e2fb3831f45
OnlyMaeko <mam900@pitt.edu>
2023-05-22 18:36:53 -0400

    Added Railroads & Utilities for properties but as their own objects. Added in all other properties to the board class

--- Board.java
+++ Board.java
@@ -2,33 +2,94 @@ package defaulta;
 
 public class Board {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//  GO
 
-		Properties Mediterranean_Avenue = new Properties("Mediterranean Avenue", 2,10,30,90,160,250, 30, 50, 50, null, 60, 1, 0, false, null, false, false,25,25);
+		Properties Mediterranean_Avenue = new Properties("Mediterranean Avenue", 2,10,30,90,160,250, 30, 50, 50, "Brown", 60, 1, 0, false, null, false, false,25,25);
 
 //		COMMUNITY CHEST
 
-		Properties Baltic_Avenue = new Properties("Baltic Avenue", 4,20,60,180,320,450, 30, 50, 50, null, 60, 3, 0, false, null, false, false,25,25);
+		Properties Baltic_Avenue = new Properties("Baltic Avenue", 4,20,60,180,320,450, 30, 50, 50, "Brown", 60, 3, 0, false, null, false, false,25,25);
 
 //		INCOME TAX
 
 //		READING RAILROAD
 
-		Properties Oriental_Avenue = new Properties("Oriental Avenue", 6,30,90,270,400,550, 50, 50, 50, null, 100, 6, 0, false, null, false, false,25,25);
+		Properties Oriental_Avenue = new Properties("Oriental Avenue", 6,30,90,270,400,550, 50, 50, 50, "Light Blue", 100, 6, 0, false, null, false, false,25,25);
 
 //		CHANCE
 
-		Properties Vermont_Avenue = new Properties("Vermont Avenue", 6,30,90,270,400,550, 50, 50, 50, null, 100, 8, 0, false, null, false, false,25,25);
+		Properties Vermont_Avenue = new Properties("Vermont Avenue", 6,30,90,270,400,550, 50, 50, 50, "Light Blue", 100, 8, 0, false, null, false, false,25,25);
 
-		Properties Connecticut_Avenue = new Properties("Connecticut Avenue", 8,40,100,300,450,600, 60, 50, 50, null, 120, 9, 0, false, null, false, false,25,25);
+		Properties Connecticut_Avenue = new Properties("Connecticut Avenue", 8,40,100,300,450,600, 60, 50, 50, "Light Blue", 120, 9, 0, false, null, false, false,25,25);
 
 //		JAIL - JUST VISITING
 
-		Properties St_Charles_Place = new Properties("St. Charles Place", 8,40,100,300,450,600, 60, 50, 50, null, 120, 9, 0, false, null, false, false,25,25);
+		Properties St_Charles_Place = new Properties("St. Charles Place", 10,50,150,450,625,750, 70, 100, 100, "Pink", 140, 11, 0, false, null, false, false,50,50);
+
+//	 Electric Company
+
+		Properties States_Avenue = new Properties("States Avenue", 10,50,150,450,625,750, 70, 100, 100, "Pink", 140, 13, 0, false, null, false, false,50,50);
+
+		Properties Virginia_Avenue = new Properties("Virginia Avenue", 12,60,180,500,700,900, 80, 100, 100, "Pink", 160, 14, 0, false, null, false, false,50,50);
+
+// Pennsylvania RAILROAD
+
+		Properties St_James_Place = new Properties("St. James Place", 14,70,200,550,750,950, 90, 100, 100, "Orange", 180, 16, 0, false, null, false, false,50,50);
+
+// 	Community CHEST
+
+		Properties Tennessee_Avenue = new Properties("Tennessee Avenue", 14,70,200,550,750,950, 90, 100, 100, "Orange", 180, 18, 0, false, null, false, false,50,50);
+
+		Properties New_York_Avenue = new Properties("New York Avenue", 16,80,220,600,800,1000, 100, 100, 100, "Orange", 200, 19, 0, false, null, false, false,50,50);
+
+// 	Free Parking
+
+		Properties Kentucky_Avenue = new Properties("Kentucky Avenue", 18,90,250,700,875,1050, 110, 150, 150, "Red", 220, 21, 0, false, null, false, false,75,75);
+
+// 	Chance
+
+		Properties Indiana_Avenue = new Properties("Indiana Avenue", 18,90,250,700,875,1050, 110, 150, 150, "Red", 220, 23, 0, false, null, false, false,75,75);
+
+		Properties Illinois_Avenue = new Properties("Illinois Avenue", 20,100,300,750,925,1100, 120, 150, 150, "Red", 240, 24, 0, false, null, false, false,75,75);
+
+// 	B & O RAILROAD
+
+		Properties Atlantic_Avenue = new Properties("Atlantic Avenue", 22,110,330,800,975,1150, 130, 150, 150, "Yellow", 260, 26, 0, false, null, false, false,75,75);
+
+		Properties Ventnor_Avenue = new Properties("Ventnor Avenue", 22,110,330,800,975,1150, 130, 150, 150, "Yellow", 260, 27, 0, false, null, false, false,75,75);
+
+// 	Water Works
+
+		Properties Marvin_Gardens = new Properties("Marvin Gardens", 24,120,360,850,1025,1200, 140, 150, 150, "Yellow", 280, 29, 0, false, null, false, false,75,75);
+
+//	GO TO JAIL
+
+		Properties Pacific_Avenue = new Properties("Pacific Avenue", 26,130,390,900,1100,1275, 150, 200, 200, "Green", 300, 31, 0, false, null, false, false,100,100);
+
+		Properties North_Carolina_Avenue = new Properties("North Carolina Avenue", 26,130,390,900,1100,1275, 150, 200, 200, "Green", 300, 32, 0, false, null, false, false,100,100);
+
+// 	Community CHest
+
+		Properties Pennsylvania_Avenue = new Properties("Pennsylvania Avenue", 28,150,450,1000,1200,1400, 160, 200, 200, "Green", 320, 34, 0, false, null, false, false,100,100);
+
+// 	Short Line
+
+// 	CHANCE
+
+		Properties Park_Place = new Properties("Park Place", 35,175,500,1100,1300,1500, 175, 200, 200, "Dark Blue", 350, 37, 0, false, null, false, false,100,100);
+
+// Luxury Tax
+
+		Properties Boardwalk = new Properties("Boardwalk", 50,200,600,1400,1700,2000, 200, 200, 200, "Dark Blue", 400, 39, 0, false, null, false, false,100,100);
+
+
+
+
+
 
 	}
-// Waterworks
-
 }
