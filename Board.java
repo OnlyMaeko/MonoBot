67aede5290508e4f1c5f82aee7c417374242eab3
OnlyMaeko <mam900@pitt.edu>
2023-05-22 02:01:06 -0400

    Update Board.java
    - Made a comment in properties

--- Board.java
+++ Board.java
@@ -5,30 +5,30 @@ public class Board {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+
 		Properties Mediterranean_Avenue = new Properties("Mediterranean Avenue", 2,10,30,90,160,250, 30, 50, 50, null, 60, 1, 0, false, null, false, false,25,25);
-		
+
 //		COMMUNITY CHEST
-		
+
 		Properties Baltic_Avenue = new Properties("Baltic Avenue", 4,20,60,180,320,450, 30, 50, 50, null, 60, 3, 0, false, null, false, false,25,25);
-		
+
 //		INCOME TAX
-		
+
 //		READING RAILROAD
-		
+
 		Properties Oriental_Avenue = new Properties("Oriental Avenue", 6,30,90,270,400,550, 50, 50, 50, null, 100, 6, 0, false, null, false, false,25,25);
 
 //		CHANCE
-		
+
 		Properties Vermont_Avenue = new Properties("Vermont Avenue", 6,30,90,270,400,550, 50, 50, 50, null, 100, 8, 0, false, null, false, false,25,25);
 
 		Properties Connecticut_Avenue = new Properties("Connecticut Avenue", 8,40,100,300,450,600, 60, 50, 50, null, 120, 9, 0, false, null, false, false,25,25);
-		
+
 //		JAIL - JUST VISITING
-		
+
 		Properties St_Charles_Place = new Properties("St. Charles Place", 8,40,100,300,450,600, 60, 50, 50, null, 120, 9, 0, false, null, false, false,25,25);
 
 	}
-
+// Waterworks
 
 }
